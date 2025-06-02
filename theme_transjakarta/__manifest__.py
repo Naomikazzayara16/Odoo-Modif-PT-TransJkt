a0f48f1c632b4d32560c12a33944a087459952bb
ikhsan Zuhri Al Ghifary <ikhsan.zuhri.tif23@polban.ac.id>
2025-06-02 12:56:10 +0700

    tema transjakarta

---
+++ theme_transjakarta/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Theme Transjakarta',
+    'version': '18.0.1.0',
+    'category': 'Theme/Backend',
+    'summary': 'Custom theme for Transjakarta: colors and fonts',
+    'description': 'A simple backend theme for Transjakarta branding.',
+    'author': 'Transjakarta IT',
+    'website': 'https://www.transjakarta.co.id',
+    'license': 'LGPL-3',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'theme_transjakarta/static/src/css/theme_transjakarta.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+} 
